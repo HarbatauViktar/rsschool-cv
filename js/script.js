e2d1854d2dbe7bb10a8c5f3ff5937c919fad88ac
HarbatauViktar <Harbatau.Viktar@gmail.com>
2021-02-24 00:16:58 +0300

    feat: add Header and Contacts

---
+++ js/script.js
@@ -0,0 +1,9 @@
+function copyPhoneNumber(document) {
+    var copyText = '+375(29)180-05-31';
+    navigator.clipboard.writeText(copyText);
+}
+
+function copyEmail(document) {
+    var copyText = 'harbatau.viktar@gmail.com';
+    navigator.clipboard.writeText(copyText);
+}
